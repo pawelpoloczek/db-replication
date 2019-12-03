8b6a66721e570f88963735ea52bd6bf66e44d017
Paweł Poloczek <30537984+pawelpoloczek@users.noreply.github.com>
2019-12-03 21:45:51 +0100

    Add doctrine (#7)

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
